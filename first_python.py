e304c64ad3d1d3d8bd1a239d254ebaadff0aad6f
Hksswnet <96300310+Hksswnet@users.noreply.github.com>
2022-05-01 13:50:34 +0800

    https://www.youtube.com/watch?v=zdMUJJKFdsU&t=4127s

---
+++ first_python.py
@@ -0,0 +1,17 @@
+#abcd
+
+print(" /|")
+
+#123
+
+print("/_|")
+
+name = "name"
+age = 89
+
+print("abc" + name)
+
+number1 = input("enter 1")
+number2 = input("enter 2")
+
+print(float(number1) + float(number2))
